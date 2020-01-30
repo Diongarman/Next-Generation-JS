1a6e702e7df9f34fd22c91ba0e4700a1a5ea900d
Dion Garman <diongarman@gmail.com>
2020-01-30 20:21:31 +0000

    ES5 Arrays

--- script.js
+++ script.js
@@ -174,7 +174,17 @@ const {firstName: a, secondName: b} = obj;
 
 console.log(a, b)
 
+//////////////////////////////////
+//LECTURE: Arrays
 
+const boxes = document.querySelectorAll('.box');
+
+//ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+});
 
 
  
