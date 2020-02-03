70105569a6c66c1c89267a622d4e26ef48747ce2
Dion Garman <diongarman@gmail.com>
2020-02-03 21:35:49 +0000

    finding index in array, ES5 vs ES6

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
 let i = 23;
 
-for(let i = 0; i<5; i++) {
-    console.log(i)
+for (let i = 0; i < 5; i++) {
+  console.log(i);
 }
 //console.log(i)
 
@@ -12,16 +12,15 @@ for(let i = 0; i<5; i++) {
 
 /*
 var is function scoped whilst let & const are block scoped
-*/ 
+*/
 
 ///////////////////////
 //Lecture Blocks and IIFEs(ugh)
 
 //ES6
 {
-
-    let a = '6ix9ine';
-    const b = 'Trippie Red';
+  let a = '6ix9ine';
+  const b = 'Trippie Red';
 }
 
 //console.log(a + b);
@@ -29,9 +28,7 @@ var is function scoped whilst let & const are block scoped
 //ES5
 
 (function() {
-
-    var c = 30;
-
+  var c = 30;
 })();
 //Below fails
 //console.log(c)
@@ -42,35 +39,30 @@ Data Privacy is much more verbose in ES5
 
 */
 
-
-
 ///////////////
 ///Lesson: Arrow Functions
 
 const years = [1989, 1990, 1991, 1992];
 
-
 //ES5
 
 var ages5 = years.map(function(year) {
-    return 2020 - year
+  return 2020 - year;
 });
 
 //console.log(ages5);
 
 //ES6
 
-let ages6 = years.map((year, index) => `Age Element ${index}: ${2020-year}`)
+let ages6 = years.map((year, index) => `Age Element ${index}: ${2020 - year}`);
 
-ages6 = years.map((year) => {
-    let now = new Date().getFullYear();
-    let age = now - year;
-    return age
-})
+ages6 = years.map(year => {
+  let now = new Date().getFullYear();
+  let age = now - year;
+  return age;
+});
 
-//console.log(ages6); 
-
-
+//console.log(ages6);
 
 ////////////////////
 ////Lesson: Arrow functions and lexical 'this'
@@ -78,72 +70,61 @@ ages6 = years.map((year) => {
 //ES5
 
 var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    var self = this;
 
-    color: 'green',
-    position: 1,
-    clickMe: function() {
+    document.querySelector('.green').addEventListener('click', function() {
+      var str =
+        'This is box number ' + self.position + ' and it is ' + self.color;
 
-        var self = this;
-
-        document.querySelector('.green').addEventListener('click', function() {
-            var str = 'This is box number ' + self.position + ' and it is ' + self.color
-
-            alert(str);
-        })
-    }
-}
+      alert(str);
+    });
+  }
+};
 
 //box5.clickMe();
 
-
 const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    document.querySelector('.green').addEventListener('click', () => {
+      let str = `This is box number ${this.position} and is ${this.color}`;
 
-    color: 'green',
-    position: 1,
-    clickMe: function() {
-
-
-        document.querySelector('.green').addEventListener('click', ()=> {
-            let str = `This is box number ${this.position} and is ${this.color}`
-
-            alert(str);
-        })
-    }
-}
+      alert(str);
+    });
+  }
+};
 
 //box6.clickMe();
 
 function Person(name) {
-    this.name = name;
-};
+  this.name = name;
+}
 //ES5
-Person.prototype.myFriends5 = function (friends) {
-
-    var arr = friends.map(function(el) {
-        return this.name + ' is friends with ' + el;
-    }
-        .bind(this)
-    );
-    console.log(arr)
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(
+    function(el) {
+      return this.name + ' is friends with ' + el;
+    }.bind(this)
+  );
+  console.log(arr);
 };
 
 //var friends = ['Ricky', 'Ashley', 'Gren', 'Nick'];
 //new Person('Dion').myFriends5(friends);
 
-
 //ES6
-Person.prototype.myFriends6 = (friends) => {
+Person.prototype.myFriends6 = friends => {
+  let arr = friends.map(el => `${this.name} + ' is friends with ' + ${el}`);
 
-    let arr = friends.map(el => `${this.name} + ' is friends with ' + ${el}`);
-
-    console.log(arr)
+  console.log(arr);
 };
 
 const friends = ['Ricky', 'Ashley', 'Gren', 'Nick'];
 new Person('Dion').myFriends6(friends);
-
-
-
 
 ///////////////////////////
 //Lecture Destructuring
@@ -158,25 +139,24 @@ var dion = ['Dion', 30];
 
 const [name, age] = dion;
 
-console.log(name, age)
+console.log(name, age);
 
 const obj = {
-    firstName: "Chuck",
-    secondName: "Norris"
+  firstName: 'Chuck',
+  secondName: 'Norris'
 };
 
-const {firstName, secondName} = obj;
-console.log(firstName, secondName)
+const { firstName, secondName } = obj;
+console.log(firstName, secondName);
 
 //if you want different key names
 
-const {firstName: a, secondName: b} = obj;
+const { firstName: a, secondName: b } = obj;
 
-console.log(a, b)
+console.log(a, b);
 
 //////////////////////////////////
 //LECTURE: Arrays
-
 
 const boxes = document.querySelectorAll('.box');
 
@@ -185,16 +165,12 @@ const boxes = document.querySelectorAll('.box');
 var boxesArr5 = Array.prototype.slice.call(boxes);
 
 boxesArr5.forEach(function(cur) {
-    cur.style.backgroundColor = 'dodgerblue';
+  cur.style.backgroundColor = 'dodgerblue';
 });
 
-
-
-
-Array.from(boxes).forEach(element => 
-    element.style.backgroundColor = 
-    'dodgerblue');
-
+Array.from(boxes).forEach(
+  element => (element.style.backgroundColor = 'dodgerblue')
+);
 
 //ES5
 /*
@@ -210,11 +186,23 @@ for(var j = 0; j< boxesArr5.length; j++) {
 //ES6
 
 for (const cur of Array.from(boxes)) {
-    if (cur.className.includes('blue')) {
-        continue;
-    }
-    cur.textContent = "I CHANGED TO BLUEFACE";
+  if (cur.className.includes('blue')) {
+    continue;
+  }
+  cur.textContent = 'I CHANGED TO BLUEFACE';
 }
 
+//ES5
 
- 
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(curr) {
+  return curr >= 18;
+});
+
+console.log(ages[full.indexOf(true)]);
+
+//ES6
+
+console.log(ages.findIndex(curr => curr >= 18));
+console.log(ages.find(curr => curr >= 18));
